e6416cf33d8cebf8b617be7f97a9f8c1744da71d
aFurkansalti <ahmetfurkansalti@gmail.com>
2023-09-06 17:57:50 +0300

    commit

--- Dersler/Day 1/helloworld.js
+++ Dersler/Day 1/helloworld.js
@@ -1,1 +1,18 @@
 console.log('Hello World but came from helloworld.js');
+
+let x = 'furkan'
+
+let y = x.split('').reverse().join('')
+
+console.log(y)
+
+let name = 'furkan'
+let z = name.charAt(0)
+let m = name[0]
+let n = name.charCodeAt(0)
+
+console.log(`Merhaba benim adım ${name}
+first char = ${z}
+first char = ${m}
+first char ASCII num= ${n}`)
+
